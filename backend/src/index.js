896fd1b5063ac8a2a09d47f52a07ac4e8707b698
Sanjay.H.M <hmsanjay812.gmail.com>
2025-07-26 01:03:01 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
-import cors from "cors";
-import path from "path";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-
-import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
@@ -18,10 +18,12 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-}))
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -35,6 +37,6 @@ if (process.env.NODE_ENV === "production") {
 }
 
 server.listen(PORT, () => {
-    console.log("server started at port - " + PORT);
+    console.log("server is running on PORT:" + PORT);
     connectDB();
-})
+});
